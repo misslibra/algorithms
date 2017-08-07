86caccd33a762d68493e1f9a3ed832c52069f539
pyclearl <cappyclear@gmail.com>
2017-08-07 10:16:33 +0800

    add solution

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 | #  | Title  | Solution | Difficulty |
 | -- | ------ | -------- | ---------- |
 | 1 | Two Sum | Python3  | Easy |
+| 7 | Reverse Integer | Python  | Easy |
---
+++ python/reverse_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# author: pyclearl
+
+class Solution(object):
+    x_min = -2147483648
+    x_max = 2147483647
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x_str = str(x)
+        if x_str.isdigit():
+            tmp = int(x_str[::-1])
+            if tmp > self.x_max:
+                return 0
+            return tmp
+        else:
+            x_int = x_str[1:]
+            x_result = -int(x_int[::-1])
+            if x_result < self.x_min:
+                return 0
+            return x_result
+
+if __name__ = '__main__':
+    pass
