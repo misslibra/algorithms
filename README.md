16933ccfd416000bea4f86180682ec54ef73e978
pyclear <cappyclear@gmail.com>
2017-08-13 22:16:16 +0800

    add atoi

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 | #  | Title  | Solution | Difficulty |
 | -- | ------ | -------- | ---------- |
 | 1 | Two Sum | Python3  | Easy |
-| 7 | Reverse Integer | Python  | Easy |
+| 7 | Reverse Integer | Python | Easy |
+| 8 | String to Integer (atoi) | Python | Medium |
---
+++ python/atoi.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# author: cappyclearl
+
+class Solution(object):
+    r_min = -2147483648
+    r_max = 2147483647
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        r_int = 0
+        sign = 1
+        str = str.lstrip()
+        if str.isdigit():
+            return self.judgeRange(int(str))
+        else:
+            for i,c in enumerate(str):
+                if i == 0:
+                    if c == '-':
+                        sign = -1
+                        continue
+                    if c == '+':
+                        sign = 1
+                        continue
+                if c.isdigit():
+                    r_int = r_int * 10  + int(c)
+                else:
+                    return self.judgeRange(sign* r_int)
+            return self.judgeRange(sign* r_int)
+        
+    def judgeRange(self, result):
+        if result > self.r_max:
+            return self.r_max
+        if result < self.r_min:
+            return self.r_min
+        return result
+            
+                    
+
+
+if __name__ == '__main__':
+    pass
+
+
+
