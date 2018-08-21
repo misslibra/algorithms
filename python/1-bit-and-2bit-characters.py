fc0e5bca47c76b1bcade04adea0bbc968da1f1d3
pyclear <cappyclear@gmail.com>
2018-08-22 07:04:34 +0800

    daily

---
+++ python/1-bit-and-2bit-characters.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+We have two special characters. The first character can be represented by
+one bit 0. The second character can be represented by two bits (10 or 11).
+
+Now given a string represented by several bits. Return whether the last
+character must be a one-bit character or not. The given string will always end with a zero.
+
+Example 1:
+Input:
+bits = [1, 0, 0]
+Output: True
+Explanation:
+The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+Example 2:
+Input:
+bits = [1, 1, 1, 0]
+Output: False
+Explanation:
+The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
+Note:
+
+1 <= len(bits) <= 1000.
+bits[i] is always 0 or 1.
+"""
+
+
+class Solution:
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+
+        >>> bits = [1, 1, 1, 0]
+        >>> s = Solution()
+        >>> s.isOneBitCharacter(bits)
+        False
+        """
+        step = 0
+        l = len(bits)
+        for i in range(1, l):
+            if bits[step] == 0:
+                step += 1
+            else:
+                step += 2
+            if step >= l - 1:  # last index is  (l-1)
+                break
+        if step == len(
+                bits
+        ):  # if len(bits) equal step go end , single element not on last
+            return False
+        else:
+            return True
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
